8b0ccdd58032399c2e71967ad61bff825682f671
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-04-11 14:14:12 +0300

    imported usermanager using gpt3 localhistory

---
+++ Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimum_Tech.Model
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Access Level { get; set; }
+
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+
+    enum Access 
+    {
+        Admin, Guest, User
+    }
+}
